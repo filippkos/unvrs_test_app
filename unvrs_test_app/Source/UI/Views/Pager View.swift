d8ecd49e9e712a4d2358532b04f02a920b880669
Filipp Kosenko <filipp.kosenko@idapgroup.com>
2023-06-13 16:51:53 +0300

    Fixed layout. Customized PagerView. PagerView cell size changes dynamically.

--- unvrs_test_app/Source/UI/Views/Pager View.swift
+++ unvrs_test_app/Source/UI/Views/Pager View.swift
@@ -12,16 +12,19 @@ protocol ScrollToPageProtocol {
 
 import UIKit
 
-class PagerView: UICollectionView, UICollectionViewDataSource, UICollectionViewDelegate {
+class PagerView: UICollectionView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
 
     // MARK: -
     // MARK: Variables
     
     var numberOfPages: Int = 4
+    var actualPage: Int = 0
     var arrayOfViews: [UIView] = []
     var scrollDelegate: ScrollToPageProtocol?
-    let containerHeight = 8
-    let containerWidth = 14
+    let containerHeight = 4
+    let disabledContainerWidth = 14
+    let enabledContainerWidth = 25
+    let spacingBetweenCells = 8
     let layout = UICollectionViewFlowLayout()
     
     // MARK: -
@@ -50,10 +53,16 @@ class PagerView: UICollectionView, UICollectionViewDataSource, UICollectionViewD
     // MARK: Public
     
     func updateViews(number: Int) {
+        self.actualPage = number
         arrayOfViews.forEach {
             $0.subviews.first?.backgroundColor = UIColor(named: "PagerGrey")
+            $0.frame.size.width = CGFloat(self.disabledContainerWidth)
+            $0.subviews.first?.frame.size.width = CGFloat(self.disabledContainerWidth)
         }
         self.arrayOfViews[number].subviews.first?.backgroundColor = UIColor(named: "PagerBlue")
+        self.arrayOfViews[number].frame.size.width = CGFloat(self.enabledContainerWidth)
+        self.arrayOfViews[number].subviews.first?.frame.size.width = CGFloat(self.enabledContainerWidth)
+        self.reloadData()
     }
     
     // MARK: -
@@ -65,42 +74,40 @@ class PagerView: UICollectionView, UICollectionViewDataSource, UICollectionViewD
     
     private func prepareLayout() {
         self.layout.scrollDirection = .horizontal
-        self.layout.sectionInset = self.getInsets()
-        self.layout.itemSize = CGSize(width: self.containerWidth * 2, height: self.containerHeight)
-        self.layout.minimumLineSpacing = 0
+        self.layout.sectionInset = self.prepareInsets()
         self.isScrollEnabled = false
     }
     
     private func prepareViews() {
         for _ in 0...self.numberOfPages {
             let view = UIView()
-            view.frame.size = CGSize(width: self.containerWidth / 2, height: self.containerHeight / 2)
-            view.layer.cornerRadius = CGFloat(self.containerHeight / 4)
-            view.backgroundColor = .white
-            
+            view.layer.cornerRadius = CGFloat(self.containerHeight / 2)
             let viewContainer = UIView()
-            viewContainer.frame.size = CGSize(width: self.containerWidth * 2, height: self.containerHeight)
+            viewContainer.frame.size = CGSize(width: self.disabledContainerWidth, height: self.containerHeight)
             viewContainer.addSubview(view)
             
             view.translatesAutoresizingMaskIntoConstraints = false
-            let containerInset = CGFloat(self.containerHeight / 4)
-            NSLayoutConstraint.activate([
-                view.trailingAnchor.constraint(equalTo: viewContainer.trailingAnchor, constant: -containerInset),
-                view.leadingAnchor.constraint(equalTo: viewContainer.leadingAnchor, constant: containerInset),
-                view.topAnchor.constraint(equalTo: viewContainer.topAnchor, constant: containerInset),
-                view.bottomAnchor.constraint(equalTo: viewContainer.bottomAnchor, constant: -containerInset)
-            ])
+            self.prepareConstraints(subView: view, superView: viewContainer, xInset: 0, yInset: 0)
             
             self.arrayOfViews.append(viewContainer)
         }
-        self.updateViews(number: 0)
+        self.updateViews(number: self.actualPage)
     }
     
-    private func getInsets() -> UIEdgeInsets {
-        let totalCellWidth = (self.containerWidth * 2) * self.numberOfPages
+    private func prepareConstraints(subView: UIView, superView: UIView, xInset: CGFloat, yInset: CGFloat) {
+        NSLayoutConstraint.activate([
+            subView.trailingAnchor.constraint(equalTo: superView.trailingAnchor, constant: -xInset),
+            subView.leadingAnchor.constraint(equalTo: superView.leadingAnchor, constant: xInset),
+            subView.topAnchor.constraint(equalTo: superView.topAnchor, constant: yInset),
+            subView.bottomAnchor.constraint(equalTo: superView.bottomAnchor, constant: -yInset)
+        ])
+    }
+    
+    private func prepareInsets() -> UIEdgeInsets {
+        let totalCellWidth = ((self.disabledContainerWidth) * (self.numberOfPages - 1)) + self.enabledContainerWidth + (self.spacingBetweenCells * (numberOfPages - 1))
         
         let horizontalInset = (self.bounds.width - CGFloat(totalCellWidth)) / 2
-        let verticalInset = (self.bounds.size.height - CGFloat(self.containerHeight)) / 2
+        let verticalInset = (self.bounds.height - CGFloat(self.containerHeight)) / 2
 
         return UIEdgeInsets(top: verticalInset, left: horizontalInset, bottom: verticalInset, right: horizontalInset)
     }
@@ -115,7 +122,7 @@ class PagerView: UICollectionView, UICollectionViewDataSource, UICollectionViewD
     }
 
     // MARK: -
-    // MARK: DataSource
+    // MARK: UICollectionView DataSource
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         self.numberOfPages
@@ -128,11 +135,27 @@ class PagerView: UICollectionView, UICollectionViewDataSource, UICollectionViewD
  
         return cell
     }
-    
+
     // MARK: -
-    // MARK: Delegate
+    // MARK: UICollectionView Delegate
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.scrollDelegate?.scrollTo(page: indexPath)
+    }
+    
+    // MARK: -
+    // MARK: UICollectionViewFlowLayout Delegate
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.row == self.actualPage {
+            return CGSize(width: self.enabledContainerWidth, height: self.containerHeight)
+        } else {
+            return CGSize(width: self.disabledContainerWidth, height: self.containerHeight)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return CGFloat(self.spacingBetweenCells)
     }
 }
